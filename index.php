bcc2b74e1f5b894c136195c57cba1fd11f2492df
nubik132 <mister.nubik@list.ru>
2023-06-17 19:08:33 +0300

    feat: places templates

--- index.php
+++ index.php
@@ -7,42 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/src/styles/style.css">
     <link rel="stylesheet" href="/src/styles/dragon.css">
+    <link rel="stylesheet" href="/src/styles/header.css">
     <title>Dragon Age Wiki</title>
 </head>
 
 <body>
     <div class="container">
-        <div class="header">
-            <a href="/" class="main-page-link">
-                <div class="dragon" id="dragon">
-                    <img src="/src/img/header/dragon/body.png" alt="body" class="dragon-menu body" id="body">
-                    <img src="/src/img/header/dragon/neck.png" alt="neck" class="dragon-menu neck" id="neck">
-                    <img src="/src/img/header/dragon/upper-arm.png" alt="upper-arm" class="dragon-menu upper-arm"
-                        id="upper-arm">
-                    <img src="/src/img/header/dragon/lower-arm.png" alt="lower-arm" class="dragon-menu lower-arm"
-                        id="lower-arm">
-                    <img src="/src/img/header/dragon/upper-head.png" alt="upper-head" class="dragon-menu upper-head"
-                        id="upper-head">
-                    <img src="/src/img/header/dragon/lower-head.png" alt="lower-head" class="dragon-menu lower-head"
-                        id="lower-head">
-                </div>
-                <div class="gradient"></div>
-            </a>
-            <div class="menu">
-                <a href="#" class="menu__item">
-                    Миссии
-                </a>
-                <a href="/states/select.php?list=characters" class="menu__item">
-                    <div>Персонажи</div>
-                </a>
-                <a href="/states/select.php?list=items" class="menu__item">
-                    <div>Предметы</div>
-                </a>
-                <a href="/states/select.php?list=combinations" class="menu__item">
-                    <div>Навыки/Способности</div>
-                </a>
-            </div>
-        </div>
+        <?php 
+            include("header.html");    
+        ?>
         <main class="main">
             <h1 class="main-h1">Dragon Age Wiki</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dignissim nisi ac facilisis
@@ -88,7 +61,6 @@
         </main>
         <footer></footer>
     </div>
-    <script src="src/scripts/Facade.js"></script>
 </body>
 
 </html>
---
+++ map/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/src/styles/style.css">
+    <link rel="stylesheet" href="/src/styles/map.css">
+    <title>Карта</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php include($_SERVER['DOCUMENT_ROOT']."/header.html"); ?>
+        <div class="map">
+            <div class="place place-1"><a href="" class="place-link"></a></div>
+            <div class="place place-2"><a href="" class="place-link"></a></div>
+            <div class="place place-3"><a href="" class="place-link"></a></div>
+            <div class="place place-4"><a href="" class="place-link"></a></div>
+            <div class="place place-5"><a href="" class="place-link"></a></div>
+            <div class="place place-6"><a href="" class="place-link"></a></div>
+            <div class="place place-7"><a href="" class="place-link"></a></div>
+            <div class="place place-8"><a href="" class="place-link"></a></div>
+            <div class="place place-9"><a href="" class="place-link"></a></div>
+            <div class="place place-10"><a href="" class="place-link"></a></div>
+            <div class="place place-11"><a href="" class="place-link"></a></div>
+            <div class="place place-12"><a href="" class="place-link"></a></div>
+            <div class="place place-13"><a href="" class="place-link"></a></div>
+            <div class="place place-14"><a href="" class="place-link"></a></div>
+            <div class="place place-15"><a href="" class="place-link"></a></div>
+            <div class="place place-16"><a href="" class="place-link"></a></div>
+            <div class="place place-17"><a href="" class="place-link"></a></div>
+            <div class="place place-18"><a href="" class="place-link"></a></div>
+            <div class="place place-19"><a href="" class="place-link"></a></div>
+            <div class="place place-20"><a href="" class="place-link"></a></div>
+            <div class="place place-21"><a href="" class="place-link"></a></div>
+            <div class="place place-22"><a href="" class="place-link"></a></div>
+            <div class="place place-23"><a href="" class="place-link"></a></div>
+            <div class="place place-24"><a href="" class="place-link"></a></div>
+            <div class="place place-25"><a href="" class="place-link"></a></div>
+            <img src="/src/img/map/map.png" alt="Карта" class="map-image">
+        </div>
+
+    </div>
+</body>
+
+</html>
