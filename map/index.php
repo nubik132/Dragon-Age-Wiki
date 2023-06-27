59fab92d3f882320107a98fb92852b6b8b81f35d
nubik132 <mister.nubik@list.ru>
2023-06-27 20:27:41 +0300

    feat: complete task

--- map/index.php
+++ map/index.php
@@ -14,31 +14,81 @@
         <script src="/src/scripts/place-mouse.js"></script>
         <?php include($_SERVER['DOCUMENT_ROOT'] . "/header.html"); ?>
         <div class="map">
-            <div class="place place-1"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-2"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-3"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-4"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-5"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-6"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-7"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-8"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-9"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-10"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-11"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-12"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-13"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-14"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-15"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-16"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-17"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-18"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-19"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-20"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-21"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-22"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-23"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-24"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
-            <div class="place place-25"><a href="" class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-1"><a href="/states/state.php?list=locations&state=Перевал в Морозных горах"
+                    class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-2"><a href="/states/state.php?list=locations&state=Общинные залы Орзаммара"
+                    class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-3"><a href="/states/state.php?list=locations&state=Сулкеров перевал"
+                    class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-4"><a href="/states/state.php?list=locations&state=Деревня Убежище"
+                    class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-5"><a href="/states/state.php?list=locations&state=Разрушенный храм"
+                    class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-6"><a href="/states/state.php?list=locations&state=Башня Круга" class="place-link"
+                    onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-7"><a href="/states/state.php?list=locations&state=Замок Редклиф" class="place-link"
+                    onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-8"><a href="/states/state.php?list=locations&state=Редклиф" class="place-link"
+                    onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-9"><a href="/states/state.php?list=locations&state=Деревня Хоннлит"
+                    class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-10"><a href="/states/state.php?list=locations&state=Пристань на озере Каленхад"
+                    class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-11"><a href="/states/state.php?list=locations&state=Земли банна Лорена"
+                    class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-12"><a href="/states/state.php?list=locations&state=Лесная поляна"
+                    class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-13"><a href="/states/state.php?list=locations&state=Лотеринг" class="place-link"
+                    onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-14"><a href="/states/state.php?list=locations&state=Где-то посреди Диких земель"
+                    class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-15"><a href="/states/state.php?list=locations&state=Забытое поле брани"
+                    class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-16"><a href="/states/state.php?list=locations&state=Покинутая низина"
+                    class="place-link" onmouseover="enter(this.children[0])" onmouseout="leave(this.children[0])"><img
+                        src="/src/img/map/select.gif" alt="select" class="select-place-img"></a></div>
+            <div class="place place-17"><a href="" class="place-link" onmouseover="enter(this.children[0])"
+                    onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select"
+                        class="select-place-img"></a></div>
+            <div class="place place-18"><a href="" class="place-link" onmouseover="enter(this.children[0])"
+                    onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select"
+                        class="select-place-img"></a></div>
+            <div class="place place-19"><a href="" class="place-link" onmouseover="enter(this.children[0])"
+                    onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select"
+                        class="select-place-img"></a></div>
+            <div class="place place-20"><a href="" class="place-link" onmouseover="enter(this.children[0])"
+                    onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select"
+                        class="select-place-img"></a></div>
+            <div class="place place-21"><a href="" class="place-link" onmouseover="enter(this.children[0])"
+                    onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select"
+                        class="select-place-img"></a></div>
+            <div class="place place-22"><a href="" class="place-link" onmouseover="enter(this.children[0])"
+                    onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select"
+                        class="select-place-img"></a></div>
+            <div class="place place-23"><a href="" class="place-link" onmouseover="enter(this.children[0])"
+                    onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select"
+                        class="select-place-img"></a></div>
+            <div class="place place-24"><a href="" class="place-link" onmouseover="enter(this.children[0])"
+                    onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select"
+                        class="select-place-img"></a></div>
+            <div class="place place-25"><a href="" class="place-link" onmouseover="enter(this.children[0])"
+                    onmouseout="leave(this.children[0])"><img src="/src/img/map/select.gif" alt="select"
+                        class="select-place-img"></a></div>
             <img src="/src/img/map/map.png" alt="Карта" class="map-image">
         </div>
 
